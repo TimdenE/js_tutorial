e7f11fb3adc5cc59fd12d226003f86eb9b58fda1
Tim den Engelsen <tim.denengelsen@hey.com>
2021-12-28 10:24:13 -0800

    finished section on functional programming

--- functional.js
+++ functional.js
@@ -21,3 +21,79 @@ function functionalUrls(elements) {
   return elements.map(element => urlify(element));
 }
 console.log(functionalUrls(states));
+
+// singles: Imperative version
+function imperativeSingles(elements) {
+    let singles = [];
+    elements.forEach(function(element) {
+      if (element.split(/\s+/).length === 1) {
+        singles.push(element);
+      }
+    });
+    return singles;
+  }
+  console.log(imperativeSingles(states));
+
+// singles: Functional version
+function functionalSingles(elements) {
+    return elements.filter(element => element.split(/\s+/).length === 1);
+  }
+  console.log(functionalSingles(states));
+
+// // EXERCISES
+// // returns all elements that contain the term Dakota.
+// function dakota(elements) {
+//     return elements.filter(element => element.includes('Dakota'))
+// }
+// console.log(dakota(states));
+
+// // returns all elements that contain two words.
+// function twoWords(elements) {
+//     return elements.filter(element => element.split(/\s+/).length === 2)
+// }
+// console.log(twoWords(states));
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function(n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+    return elements.reduce((total, n) => { return total += n; });
+  }
+  console.log(functionalSum(numbers))
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+    let lengths = {};
+    elements.forEach(function(element) {
+      lengths[element] = element.length;
+    });
+    return lengths;
+  }
+  console.log(imperativeLengths(states));
+
+
+// lengths: Functional solution
+function functionalLengths(elements) {
+    return elements.reduce((lengths, element) => {
+                            lengths[element] = element.length;
+                            return lengths;
+                          }, {});
+  }
+  console.log(functionalLengths(states));
+
+// EXERCISE
+// // sum: Functional solution
+// function functionalProduct(elements) {
+//     return elements.reduce((total, n) => { return total *= n; });
+//   }
+//   console.log(functionalProduct(numbers))
